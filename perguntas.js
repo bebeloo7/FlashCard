b079aa5c790deb9b643158d5943b9078b2fa117e
bebeloo7 <00001085119270sp@al.educacao.sp.gov.br>
2024-11-05 18:08:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tempo livre',
+    'O que gosto de fazer?',
+    'Estudar'
+)
+
+criaCartao(
+    'Profissão',
+    'QUal profissão quero fazer?',
+    'Advogada'
+)
+
+criaCartao(
+    'Aniversario',
+    'Qual minha data de aniversario?',
+    '19/12'
+)
+
+criaCartao(
+    'Comida',
+    'Qual comida mais gosto?',
+    'Lasanha'
+)
